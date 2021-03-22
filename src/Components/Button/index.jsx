ceb0ece959503d1c54af8ba4682499583e313e23
Julys Martins <julystube@gmail.com>
2021-03-22 00:03:25 -0300

    migrando para hook

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,18 +1,38 @@
 import './styles.css'
 
-import React, { Component } from 'react'
+import React from 'react'
 
-export  class Button extends Component {
-  render() {
-    const {text,onClick,disabled} = this.props;
-    return (
-          <button
-           className='button'
-          onClick={onClick}
-          disabled={disabled}
-          >
-            {text}
-          </button>
-    )
-  }
+import './styles.css';
+
+
+
+export  function Button({text, onClick, disabled }) {
+  return (
+    <button
+      className='button'
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  )
 }
+
+
+
+
+/* export class Button extends Component {
+  render() {
+    const { text, onClick, disabled } = this.props;
+
+    return (
+      <button
+        className='button'
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {text}
+      </button>
+    );
+  }
+} */
--- src/templates/Home/Index.jsx
+++ src/templates/Home/Index.jsx
@@ -1,13 +1,82 @@
 import './Styles.css';
-import { Component } from 'react';
+import {  useCallback, useEffect, useState } from 'react';
 import { Posts } from '../../Components/Posts/index'
 import { loadPosts } from '../../ultils/load-posts';
 import { Button } from '../../Components/Button'
 import { TextInput } from '../../Components/TextInput';
-class Home extends Component {
+
+ function Home() {
+
+    const [posts, setPosts] = useState([]);
+    const [allPosts, setAllposts] = useState([]);
+    const [page,setPage ] = useState(0);
+    const [postsPerPage,] = useState(2);
+    const [searchValue, setSearchValue] = useState("");
+
+    const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+        const postsAndPhotos = await loadPosts();
+        setPosts(postsAndPhotos.slice(page, postsPerPage))
+        setAllposts(postsAndPhotos)
+    }, [])
+    useEffect(() => {
+        handleLoadPosts(0, postsPerPage);
+
+    }, [handleLoadPosts, postsPerPage])
+   const loadMorePosts = () => {
+        const nextPage = page + postsPerPage;
+        const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+        posts.push(...nextPosts)
+        setPosts(posts);
+        setPage(nextPage)
+    }
+    const handleChange = (e) => {
+        const { value } = e.target;
+        setSearchValue(value);
+    }
+    
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue ? allPosts.filter(post => {
+        return post.title.toLowerCase().includes(
+            searchValue.toLowerCase()
+        )
+    }) : posts;
+    return (
+        <section className="container">
+      <div className="search-container">
+        {!!searchValue && (
+          <h1>Search value: {searchValue}</h1>
+        )}
+
+        <TextInput searchValue={searchValue} handleChange={handleChange} />
+      </div>
+
+      {filteredPosts.length > 0 && (
+        <Posts posts={filteredPosts} />
+      )}
+
+      {filteredPosts.length === 0 && (
+        <p>Não existem posts =(</p>
+      )}
+
+      <div className="button-container">
+        {!searchValue && (
+          <Button
+            text="Load more posts"
+            onClick={loadMorePosts}
+            disabled={noMorePosts}
+          />
+        )}
+      </div>
+    </section>
+     
+    );
+}
+
+/* class Home2 extends Component {
     state = {
         posts: [],
-        allPost: [],
+        allPosts: [],
         page: 0,
         postsPerPage: 10,
         searchValue: ' '
@@ -22,7 +91,7 @@ class Home extends Component {
         const postsAndPhotos = await loadPosts();
         this.setState({
             posts: postsAndPhotos.slice(page, postsPerPage),
-            allPost: postsAndPhotos,
+            allPosts: postsAndPhotos,
         })
     }
 
@@ -30,15 +99,15 @@ class Home extends Component {
         const {
             page,
             postsPerPage,
-            allPost,
+            allPosts,
             posts,
         } = this.state;
 
         const nextPage = page + postsPerPage;
-        const nextPosts = allPost.slice(nextPage, nextPage + postsPerPage);
+        const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
         posts.push(...nextPosts)
 
-        this.setState({ posts, page: nextPosts })
+        this.setState({ posts, page: nextPosts }) o erro era aqui
     }
     handleChange = (e) => {
         const { value } = e.target;
@@ -47,10 +116,10 @@ class Home extends Component {
     }
 
     render() {
-        const { posts, page, postsPerPage, allPost, searchValue } = this.state;
-        const noMorePost = page + postsPerPage >= allPost.length;
+        const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
+        const noMorePost = page + postsPerPage >= allPosts.length;
 
-        const filteredPosts = !!searchValue ? allPost.filter(post => {
+        const filteredPosts = !!searchValue ? allPosts.filter(post => {
             return post.title.toLowerCase().includes(
                 searchValue.toLowerCase()
             )
@@ -84,22 +153,22 @@ class Home extends Component {
 
 
                 <div className="button-container">
-              
-                        <Button
-                        text="Load More posts"
-                        onClick={this.loadMorePosts}
-                        disabled={noMorePost}
-                        />
-                     
-                
-                    
-                    
+                    {
+                        !searchValue && (
+                            <Button
+                                text="Load More posts"
+                                onClick={this.loadMorePosts}
+                                disabled={noMorePost}
+
+                            />
+                        )
+                    }
                 </div>
             </section>
         );
     }
 }
-
+ */
 
 
 //function Home() {
