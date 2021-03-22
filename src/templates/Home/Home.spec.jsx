0edd71b97e59ebfaff0c3d213c2974020dbb1b03
Julys Martins <julystube@gmail.com>
2021-03-22 15:45:44 -0300

    adicionando testes com jest

---
+++ src/templates/Home/Home.spec.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Home from './Index';
+describe('<Home />', () => {
+  it('is a dummy test', () => {
+    expect(1).toBe(1);
+  })
+
+})
